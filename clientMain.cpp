6634575277fef1a423cf531d7ac5bcaace85cef1
Federico Jure <fjure@atixlabs.com>
2020-06-03 00:17:10 -0300

    first commit

---
+++ clientMain.cpp
@@ -0,0 +1,13 @@
+
+
+#define CLIENT_SUCESS 0
+
+int main(int argc, char* argv[]) {
+    //handleo de argumentos de ejecución
+
+    //conexion con servidor
+
+    //empieza a escuchar input del usuario y handlea cada uno 
+
+    return CLIENT_SUCESS;
+}
---
+++ serverMain.cpp
@@ -0,0 +1,8 @@
+
+#define SERVER_SUCESS 0
+#define SERVER_ERROR 1
+
+int main(int argc, char* argv[]) {
+
+    return SERVER_SUCESS;
+}
