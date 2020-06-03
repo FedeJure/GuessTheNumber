7a9762c44ba8b54db02b363e51bba4d254f9c35e
Federico Jure <fjure@atixlabs.com>
2020-06-03 20:18:56 -0300

    creo socketServer

--- commonSocket.cpp
+++ commonSocket.cpp
@@ -6,21 +6,62 @@
 #include <string>
 #include "./commonSocket.h"
 
+common::Socket::Socket(std::string& providedService)
+    : service(providedService){}
 
-common::Socket::Socket(std::string& service) {
-    acquireFd();
+common::Socket::~Socket() {
+    shutdown(fd, SHUT_RDWR);
+    close(fd);
 }
 
-common::Socket::Socket(std::string& host, std::string& service) {
-    acquireFd();
-}
+void common::Socket::send(char* buffer, size_t length) {}
+void common::Socket::receive(char* buffer, size_t length) {}
 
 void common::Socket::acquireFd() {
-    if ((this->fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+    if ((fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
         throw std::exception();
 }
 
-common::Socket::~Socket() {
-    shutdown(this->fd, SHUT_RDWR);
-    close(this->fd);
+common::SocketServer::SocketServer(std::string& service, int maxConnections) 
+    : common::Socket(service), maxConnections(maxConnections) {
+    acquireFd();
+    setOptions();
+    bindSocket();
+    startListen();
+}
+
+int common::SocketServer::acceptConnection() {
+    size_t socket_size = sizeof(ip4addr);
+    int accepted = accept(fd, (struct sockaddr*) &ip4addr,
+        (socklen_t*)&socket_size);
+    if (accepted < -1) throw std::exception();
+    connected_clients.push_back(accepted);
+    return accepted;
+}
+
+
+void common::SocketServer::setOptions() {
+    ip4addr.sin_family = AF_INET;
+    ip4addr.sin_addr.s_addr = INADDR_ANY;
+    ip4addr.sin_port = htons(std::stoi(service));
+}
+
+void common::SocketServer::bindSocket() {
+    int binded = bind(fd, (const struct sockaddr*)&ip4addr,
+        sizeof(ip4addr));
+    if (binded == -1) {
+        throw std::exception();
+    }
+}
+
+void common::SocketServer::startListen() {
+    if (listen(fd, maxConnections) < 0) {
+        throw std::exception();
+    }
+}
+
+common::SocketServer::~SocketServer() {
+    for (int clients : connected_clients) {
+        close(clients);
+    }
 }
--- commonSocket.h
+++ commonSocket.h
@@ -1,19 +1,39 @@
 #ifndef SOCKET_H_
 #define SOCKET_H_
 #include <string>
+#include <vector>
+#include <netinet/in.h>
 
 namespace common
 {
     class Socket {
+        protected:
         int fd;
+        std::string service;
+        sockaddr_in ip4addr;
+        void acquireFd();
 
         public:
         explicit Socket(std::string& service);
-        Socket(std::string& host, std::string& service);
-        ~Socket();
+        virtual void send(char* buffer, size_t length);
+        virtual void receive(char* buffer, size_t length);
+        virtual ~Socket();
+    };
+
+    class SocketServer : Socket {
+        int maxConnections;
+        std::vector<int> connected_clients;
+
+        public:
+        SocketServer(std::string& service, int maxConnection);
+        SocketServer(std::string& host, std::string& service);
+        int acceptConnection();
+        ~SocketServer();
 
         private:
-        void acquireFd();
+        void setOptions();
+        void bindSocket();
+        void startListen();
     };
 } // namespace server
 
